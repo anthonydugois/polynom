646ec9b9235538cb9836234ef506294e8a81a7ba
Anthony Dugois <dev.anthonydugois@gmail.com>
2015-10-28 15:06:02 +0100

    deleted postcss input style

--- webpack.config.js
+++ webpack.config.js
@@ -80,7 +80,6 @@ export default {
             postcssMediaMinmax,
             postcssBrandColors,
             postcssColorFunction,
-            postcssInputStyle,
             postcssUrl,
         ]
     }
