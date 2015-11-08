c0581c9a0b0c302ee95f50a71092d8448771b634
Anthony Dugois <dev.anthonydugois@gmail.com>
2015-11-08 13:45:16 +0100

    Remove react-router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { render } from "react-dom"
 
-import routes from "./routes"
+import App from "App"
 
-render(routes, document.querySelector("#app"))
+render(<App />, document.querySelector("#app"))
